deed0fc031382584ab004830dea7c5b8c59964d9
Stefan Loa <steloa@protonmail.com>
2026-08-13 15:25:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const VERIFY_TOKEN = "eden_verify_123";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
